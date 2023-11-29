7eda49021570446cac04adb8814397121736fb29
Mark Linder <mark.linder@endress.com>
2023-11-29 15:39:10 +0100

    Adding Example for Code Comment

--- Quixduell/Quixduell.ServiceLayer/Services/SayHelloService.cs
+++ Quixduell/Quixduell.ServiceLayer/Services/SayHelloService.cs
@@ -1,10 +1,26 @@
 ﻿namespace Quixduell.ServiceLayer.Services
 {
+    /// <summary>
+    /// <c>SayHelloService</c> is used as an example to show code documentation
+    /// </summary>
     public class SayHelloService
     {
-        public string SayHello()
+        /// <summary>
+        /// Variables need to be descriped 
+        /// </summary>
+        private readonly string _serviceName = nameof(SayHelloService);
+
+
+        /// <summary>
+        /// Construct hello String 
+        /// </summary>
+        /// <exception cref="ArgumentNullException">Throws when value is null</exception>
+        /// <param name="toUser">Name of user</param>
+        /// <returns>Hello String</returns>
+        public string SayHello(string toUser)
         {
-            return "Hello from Github to Azure CI";
+            ArgumentNullException.ThrowIfNull(toUser);
+            return $"Hello from {_serviceName} to {toUser}";
         }
 
     }
