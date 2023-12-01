c29c0e1c466ee22d2ff987aab55cc0a04c80ea68
Mark Linder <mark.linder@endress.com>
2023-12-01 14:26:22 +0100

    First Prototype for Adding new Lernsets

---
+++ Quixduell/Quixduell.Blazor/Model/LernsetPagesModel/CreateLernset.cs
@@ -0,0 +1,36 @@
+﻿using Quixduell.ServiceLayer.DataAccessLayer.Model;
+using System.ComponentModel.DataAnnotations;
+
+namespace Quixduell.Blazor.Model.LernsetPagesModel
+{
+    /// <summary>
+    /// Request Class for Validation of new Lernset
+    /// </summary>
+    public class CreateLernset
+    {
+        [Required]
+        public Category? Category { get; set; }
+        public List<AppUser> Contributor { get; set; } = new List<AppUser>();
+
+        public List<Question> Questions { get; set; } = new List<Question>();
+
+        public AppUser Creator { get;}
+
+
+        public string GetContributortoString
+        {
+            get
+            {
+                return String.Join(",", Contributor);
+            }
+        }
+
+        public CreateLernset(AppUser creator)
+        {
+
+            Creator = creator;
+
+        }
+
+    }
+}
---
+++ Quixduell/Quixduell.Blazor/Model/QuestionModel/EditQuestion.cs
@@ -0,0 +1,33 @@
+﻿using Quixduell.ServiceLayer.DataAccessLayer.Model;
+using System.ComponentModel.DataAnnotations;
+
+namespace Quixduell.Blazor.Model.QuestionModel
+{
+    /// <summary>
+    /// Request Class for Edit a Question
+    /// </summary>
+    public class EditQuestion
+    {
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        public Question_Type Question_Type { get; set; }
+
+        [Required]
+        [StringLength(1000)]
+        public string QuestionText { get; set; } = string.Empty;
+
+        [Required]
+        public List<Answer> Answers { get; set; }
+
+
+        public EditQuestion(Question question)
+        {
+            Name = question.Name;
+            QuestionText = question.QuestionText;
+            Question_Type = question.Question_Type;
+            Answers = question.Answer;
+        }
+    }
+}
