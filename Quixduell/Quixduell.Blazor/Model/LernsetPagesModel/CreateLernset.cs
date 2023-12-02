9b2ccd3b08d16bd7c9d7b4192157ec645c2dc8bf
hansefred <hansefredchen@gmail.com>
2023-12-02 23:08:33 +0100

    Adding Lernset Name Adding Complex Validation to Question Component Adding some Bootstrap

--- Quixduell/Quixduell.Blazor/Model/LernsetPagesModel/CreateLernset.cs
+++ Quixduell/Quixduell.Blazor/Model/LernsetPagesModel/CreateLernset.cs
@@ -12,6 +12,12 @@ namespace Quixduell.Blazor.Model.LernsetPagesModel
         public Category? Category { get; set; }
         public List<AppUser> Contributor { get; set; } = new List<AppUser>();
 
+
+        [Required]
+        [MinLength(5)]
+        [MaxLength(50)]
+        public string Name { get; set; } = String.Empty;
+
         [MinLength(1)]
         public List<Question> Questions { get; set; } = new List<Question>();
 
