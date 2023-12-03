6000b6002c23059b48ab3a36588bcb9f0682ed47
hansefred <hansefredchen@gmail.com>
2023-12-03 10:18:03 +0100

    Adding New Category Edit Lernset Permission Check on Edit Lernset

---
+++ Quixduell/Quixduell.Blazor/Model/LernsetPagesModel/ChangeLernset.cs
@@ -0,0 +1,39 @@
+﻿using Quixduell.ServiceLayer.DataAccessLayer.Model;
+using System.ComponentModel.DataAnnotations;
+
+namespace Quixduell.Blazor.Model.LernsetPagesModel
+{
+    public abstract class ChangeLernset
+    {
+        [Required]
+        public Category? Category { get; set; }
+        public List<AppUser> Contributor { get; set; } = new List<AppUser>();
+
+
+        [Required]
+        [MinLength(5)]
+        [MaxLength(50)]
+        public string Name { get; set; } = String.Empty;
+
+        [MinLength(1)]
+        public List<Question> Questions { get; set; } = new List<Question>();
+
+        [Required]
+        public AppUser Creator { set; get; }
+
+
+        public string GetContributortoString
+        {
+            get
+            {
+                return String.Join(",", Contributor);
+            }
+        }
+
+        public ChangeLernset(AppUser creator)
+        {
+            Creator = creator;
+        }
+
+    }
+}
--- Quixduell/Quixduell.Blazor/Model/LernsetPagesModel/CreateLernset.cs
+++ Quixduell/Quixduell.Blazor/Model/LernsetPagesModel/CreateLernset.cs
@@ -6,39 +6,10 @@ namespace Quixduell.Blazor.Model.LernsetPagesModel
     /// <summary>
     /// Request Class for Validation of new Lernset
     /// </summary>
-    public class CreateLernset
+    public class CreateLernset : ChangeLernset
     {
-        [Required]
-        public Category? Category { get; set; }
-        public List<AppUser> Contributor { get; set; } = new List<AppUser>();
+        public CreateLernset(AppUser appUser) : base(appUser) { }
 
-
-        [Required]
-        [MinLength(5)]
-        [MaxLength(50)]
-        public string Name { get; set; } = String.Empty;
-
-        [MinLength(1)]
-        public List<Question> Questions { get; set; } = new List<Question>();
-
-        [Required]
-        public AppUser Creator { get;}
-
-
-        public string GetContributortoString
-        {
-            get
-            {
-                return String.Join(",", Contributor);
-            }
-        }
-
-        public CreateLernset(AppUser creator)
-        {
-
-            Creator = creator;
-
-        }
 
     }
 }
