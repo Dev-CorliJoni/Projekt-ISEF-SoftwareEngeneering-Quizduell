cbe6061675d213d3a2c4e1e7294dfe1775100dfc
hansefred <hansefredchen@gmail.com>
2023-12-03 10:18:03 +0100

    Adding New Category Edit Lernset Permission Check on Edit Lernset

---
+++ Quixduell/Quixduell.Blazor/Model/LernsetPagesModel/EditLernset.cs
@@ -0,0 +1,20 @@
+﻿using Quixduell.ServiceLayer.DataAccessLayer.Model;
+
+namespace Quixduell.Blazor.Model.LernsetPagesModel
+{
+    public class EditLernset : ChangeLernset
+    {
+        public Guid ExistingLernsetID { get; set; }
+
+        public EditLernset(Lernset lernset) : base(lernset.Creator) 
+        {
+            ExistingLernsetID = lernset.ID;
+            Category = lernset.Category;
+            Contributor = lernset.Contributors;
+            Name = lernset.Name;
+            Questions = lernset.Questions;
+
+        }
+
+    }
+}
