122e19699698028ce86ced0795a061b7ef140186
Jonathan Haueis <jonathan.haueis@endress.com>
2023-12-12 13:37:24 +0100

    get user over constructor

--- Quixduell/Quixduell.ServiceLayer/ServiceLayer/GlobalSearch.cs
+++ Quixduell/Quixduell.ServiceLayer/ServiceLayer/GlobalSearch.cs
@@ -13,10 +13,12 @@ namespace Quixduell.ServiceLayer.ServiceLayer
     public class GlobalSearch
     {
         private readonly StudysetDataAccess _studysetDataAccess;
+        private readonly User _user;
 
-        internal GlobalSearch(StudysetDataAccess studysetDataAccess)
+        internal GlobalSearch(StudysetDataAccess studysetDataAccess, User user)
         {
             _studysetDataAccess = studysetDataAccess;
+            _user = user;
         }
 
         public async Task<List<Studyset>> Search(string name)
@@ -24,12 +26,12 @@ namespace Quixduell.ServiceLayer.ServiceLayer
              return await (await _studysetDataAccess.LoadTopByNameAsync(name)).ToListAsync();
         }
 
-        public async Task SaveStudyset(User user, Studyset studyset)
+        public async Task SaveStudyset(Studyset studyset)
         {
-            if(user.StudysetConnections.Any(sc => sc.Studyset.Name == studyset.Name) == false)
+            if(_user.StudysetConnections.Any(sc => sc.Studyset.Name == studyset.Name) == false)
             {
-                var studysetUserConnection = new UserStudysetConnection(user, studyset, new Rating());
-                user.StudysetConnections.Add(studysetUserConnection);
+                var studysetUserConnection = new UserStudysetConnection(_user, studyset);
+                _user.StudysetConnections.Add(studysetUserConnection);
                 studyset.Connections.Add(studysetUserConnection);
                 await _studysetDataAccess.UpdateAsync(studyset);
             }
