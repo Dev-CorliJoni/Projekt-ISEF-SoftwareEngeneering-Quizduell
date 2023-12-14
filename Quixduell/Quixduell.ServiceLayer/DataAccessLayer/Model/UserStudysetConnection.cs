7d340e029cb2628762b5d57ccd6a9caa39d350a8
Mark Linder <mark.linder@endress.com>
2023-12-14 09:57:40 +0100

    AddLastSeen

--- Quixduell/Quixduell.ServiceLayer/DataAccessLayer/Model/UserStudysetConnection.cs
+++ Quixduell/Quixduell.ServiceLayer/DataAccessLayer/Model/UserStudysetConnection.cs
@@ -2,15 +2,40 @@
 {
     public class UserStudysetConnection : IdModel
     {
+
+        private bool _isStored;
+        private float _highscore;
+
+
         public User User { get; set; }
         public Studyset Studyset { get; set; }
-
-
-        public bool IsStored { get; set; }
         public Rating Rating { get; set; }
-        public float Highscore { get; set; }
 
-        private UserStudysetConnection() { }
+        public bool IsStored
+        {
+            get => _isStored;
+            set
+            {
+                LastSeen = DateTime.Now;
+                _isStored = value;
+            }
+        }
+        public float Highscore
+        {
+            get => _highscore;
+            set
+            {
+                LastSeen = DateTime.Now;
+                _highscore = value;
+            }
+        }
+
+        public DateTime LastSeen { get; set; }
+
+        private UserStudysetConnection() 
+        {
+            LastSeen = DateTime.Now;
+        }
 
         public UserStudysetConnection(User user, Studyset studyset, bool isStored) : this(user, studyset, isStored, new Rating(), 0)
         {
@@ -20,7 +45,6 @@
         {
             User = user;
             Studyset = studyset;
-
             IsStored = isStored;
             Rating = rating;
             Highscore = highscore;
