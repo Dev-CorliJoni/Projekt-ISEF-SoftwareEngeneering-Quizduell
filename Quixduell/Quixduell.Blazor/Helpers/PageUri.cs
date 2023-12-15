7736e53fd5d92a441151ead27fdc2f6e8393bd76
Mark Linder <mark.linder@endress.com>
2023-12-15 16:06:46 +0100

    Fix Link

--- Quixduell/Quixduell.Blazor/Helpers/PageUri.cs
+++ Quixduell/Quixduell.Blazor/Helpers/PageUri.cs
@@ -2,6 +2,7 @@
 {
     public static class PageUri
     {
+        public const string Index = "/";
         public const string LoginPage = "/Identity/Account/Login";
         public const string EditStudysetPage = "/editstudyset";
         public const string ViewStudysetPage = "/viewstudyset";
