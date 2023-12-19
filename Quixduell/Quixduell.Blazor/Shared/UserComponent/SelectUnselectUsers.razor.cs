cc141701da04eeb640619835983c0b461cedc864
Mark Linder <mark.linder@endress.com>
2023-12-19 12:00:42 +0100

    Add Select Unselect User

---
+++ Quixduell/Quixduell.Blazor/Shared/UserComponent/SelectUnselectUsers.razor.cs
@@ -0,0 +1,71 @@
+﻿
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Identity;
+using Quixduell.ServiceLayer.DataAccessLayer.Model;
+
+namespace Quixduell.Blazor.Shared.UserComponent
+{
+    public partial class SelectUnselectUsers
+    {
+        [Inject]
+        public IServiceProvider ServiceProvider { get; set; } = default!;
+
+        [Inject]
+        public UserManager<User> UserManager { get; set; } = default!;
+
+        [Parameter]
+        public List<User>? Value { get; set; }
+
+        [Parameter]
+        public EventCallback<List<User>> ValueChanged { get; set; }
+
+        private List<User>? _displayedAppUsers;
+        private List<User>? _appUsers;
+        private string _searchString = "";
+
+
+        protected override Task OnInitializedAsync()
+        {
+            using (var scope = ServiceProvider.CreateScope())
+            {
+                var userManager = scope.ServiceProvider.GetService<UserManager<User>>();
+                _appUsers = userManager!.Users.ToList();
+                SearchUser();
+            }
+            return base.OnInitializedAsync();
+        }
+
+        protected override void OnParametersSet()
+        {
+            if (Value is null) 
+            {
+                Value = new List<User>();
+            }
+            base.OnParametersSet();
+        }
+
+        private void SearchUser()
+        {
+            if (_appUsers is not null)
+            {
+                _displayedAppUsers = _appUsers.FindAll(o => (o.Email ?? "").ToLower().Contains(_searchString.ToLower()));
+                StateHasChanged();
+            }
+        }
+
+        private async Task DeleteUser(User appUser)
+        {
+            var user = UserManager.Users.First(o => o.Id == appUser.Id);
+            Value?.Remove(user);
+            await ValueChanged.InvokeAsync(Value);
+        }
+
+
+        private async Task AddUser(User appUser)
+        {
+            var user = UserManager.Users.First(o => o.Id == appUser.Id);
+            Value?.Add(user);
+            await ValueChanged.InvokeAsync(Value);
+        }
+    }
+}
