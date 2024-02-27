abb16ae8e0d008443dcf04e997e3a3077e982011
hansefred <hansefredchen@gmail.com>
2024-02-27 23:19:38 +0100

    Initial

--- Quixduell/Quixduell.ServiceLayer/DataAccessLayer/Model/User.cs
+++ Quixduell/Quixduell.ServiceLayer/DataAccessLayer/Model/User.cs
@@ -4,12 +4,21 @@ namespace Quixduell.ServiceLayer.DataAccessLayer.Model
 {
     public class User : IdentityUser
     {
+
+        public List<Studyset> CreatedStudysets { get; set; }
+        public List<Studyset> ContributedStudysets { get; set; }
+
+        public List<Studyset> UsersRequestedToBecomeContributors { get; set; }
         public List<UserStudysetConnection> StudysetConnections { get; set; }
+
+       
 
 
         public User()
         {
             StudysetConnections = new List<UserStudysetConnection>();
+            CreatedStudysets = new List<Studyset>();
+            ContributedStudysets = new List<Studyset>();
         }
     }
 }
