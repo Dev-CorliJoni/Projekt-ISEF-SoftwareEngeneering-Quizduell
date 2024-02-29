f32a19b4943984b167a91afb926e9a65517e4e6f
Jonathan Haueis <jonathan.haueis@endress.com>
2024-02-29 11:05:21 +0100

    comment IssueManager

--- Quixduell/Quixduell.ServiceLayer/ServiceLayer/IssueManager.cs
+++ Quixduell/Quixduell.ServiceLayer/ServiceLayer/IssueManager.cs
@@ -4,26 +4,39 @@ using Quixduell.ServiceLayer.Services.MailSender;
 
 namespace Quixduell.ServiceLayer
 {
+    /// <summary>
+    /// Represents a class for managing reported issues.
+    /// </summary>
     public class IssueManager
     {
         private readonly IMailSender _mailSender;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="IssueManager"/> class.
+        /// </summary>
+        /// <param name="mailSender">The mail sender service.</param>
         public IssueManager(IMailSender mailSender)
         {
             _mailSender = mailSender;
         }
 
-        public async Task ReportIssueAsync (Studyset studyset,BaseQuestion reportedQuestion,User reporter, string Issue)
+        /// <summary>
+        /// Reports an issue related to a study set.
+        /// </summary>
+        /// <param name="studyset">The study set related to the issue.</param>
+        /// <param name="reportedQuestion">The reported question.</param>
+        /// <param name="reporter">The user reporting the issue.</param>
+        /// <param name="issue">The description of the issue.</param>
+        public async Task ReportIssueAsync(Studyset studyset, BaseQuestion reportedQuestion, User reporter, string issue)
         {
             var sub = $"Issue for Studyset {studyset.Name} was reported";
-            var body = $"{reporter.Email} has reported the issue: {Issue}";
+            var body = $"{reporter.Email} has reported the issue: {issue}";
             await _mailSender.SendMailAsync(null, studyset.Creator.Email, sub, body);
 
-            foreach (var cont in studyset.Contributors)
+            foreach (var contributor in studyset.Contributors)
             {
-                await _mailSender.SendMailAsync(null, studyset.Creator.Email, sub, body);
+                await _mailSender.SendMailAsync(null, contributor.Email, sub, body);
             }
         }
-       
     }
 }
