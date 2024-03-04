51d29ebdb9ae0764e320d3746dd81725549bbedb
Mark Linder <mark.linder@endress.com>
2024-03-04 09:46:23 +0100

    CreateEditCategoryFormModel Name MinimumLength 2

--- Quixduell/Quixduell.Blazor/EditFormModel/CreateEditCategoryFormModel.cs
+++ Quixduell/Quixduell.Blazor/EditFormModel/CreateEditCategoryFormModel.cs
@@ -5,7 +5,7 @@ namespace Quixduell.Blazor.EditFormModel
 {
     public class CreateEditCategoryFormModel
     {
-        [StringLength(50, MinimumLength = 5, ErrorMessage = "Der Name muss eine Länge zwischen {1} und {2} haben")]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Der Name muss eine Länge zwischen {1} und {2} haben")]
         public string Name { get; set; } = string.Empty;
         public Guid Id { get; set; } = Guid.Empty;
 
